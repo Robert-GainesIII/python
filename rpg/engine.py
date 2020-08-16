f7143540cf7f0e6eec9cd5f1ef79ad7bb3487cdf
DESKTOP-G65IP2H\ITJoe <gaines.robert.edgar@gmail.com>
2020-08-16 07:12:55 -0700

    New commit added obstacle.py and many things to engine

--- rpg/engine.py
+++ rpg/engine.py
@@ -1,9 +1,16 @@
 import pygame
+from obstacle import platform as plat
 from player import player as P
+
 
 pygame.init()
 
-screen = pygame.display.set_mode((500,500))
+SCREEN_W = 600
+SCREEN_H = 600
+
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((SCREEN_W,SCREEN_H))
 
 background = pygame.image.load("images/bg.png")
 
@@ -12,34 +19,71 @@ pygame.display.set_caption("Rpg")
 playingGame = True
 
 p1 = P("bobby", "spellsword", screen)
+obstacles = []
+obstacles.append(plat("platform 1 is a baby", 200, 500, screen))
 
 def redraw():
-    screen.blit(background, (0,0))
-    p1.draw()
-    pygame.display.update()
+	screen.blit(background, (0,0))
+	for o in obstacles:
+		o.draw()
+	p1.draw()
+	pygame.display.update()
 
+	
+def checkCollisions(thing1, thing2):
+	if(abs(thing1.x - thing2.x) < 25 and abs(thing1.y - thing2.y < 25)):
+		return True
+	else:
+		return False
+	
+
+def physics():
+	p1.move()
+	if p1.dx > 0:
+		p1.dx -= 2
+	elif p1.dx < 0:
+		p1.dx += 2
+	else:
+		p1.setdX(0)
+	for o in obstacles:
+		if checkCollisions(p1, o):
+			p1.setdY(0)
+			p1.setOnPlatform(True)
+			p1.isJumping = False
+			p1.doubleJump = False
+	if p1.getY() < 400 and p1.onPlatform != True:
+		p1.changeY(10) 
+	else:
+		p1.isJumping = False
+		p1.doubleJump = False
+		p1.setdY(0)
+		p1.setY(400)
+	
 
 while playingGame:
 
-    pygame.time.delay(50)
+	clock.tick(30)
 
-    for event in pygame.event.get():
+	for event in pygame.event.get():
 
-        if event.type == pygame.QUIT:
-            playingGame = False
+		if event.type == pygame.QUIT:
+			playingGame = False
 
-    keys = pygame.key.get_pressed()
+	keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_w]:
-        p1.moveY(-5)
-    elif keys[pygame.K_a]:
-        p1.moveX(-5)
-    elif keys[pygame.K_s]:
-        p1.moveY(5)
-    elif keys[pygame.K_d]:
-        p1.moveX(5)
-
-
-    redraw()
+	if keys[pygame.K_a]:
+		p1.changeX(-4)
+	if keys[pygame.K_d]:
+		p1.changeX(4)
+	if keys[pygame.K_SPACE] and p1.isJumping != True:
+		p1.changeY(-60)
+		p1.setLastJump(pygame.time.get_ticks())
+		p1.isJumping = True
+	elif keys[pygame.K_SPACE] and p1.isJumping == True and p1.doubleJump == False and (pygame.time.get_ticks()- p1.timeSinceJump > 200):
+		p1.changeY(-60)
+		p1.doubleJump = True
+	physics()
+	redraw()
 
 pygame.quit()
+
--- rpg/player.py
+++ rpg/player.py
@@ -4,23 +4,62 @@ image = pygame.image.load('images/rpgGuy.png')
 
 class player(object):
 
-    def __init__(self, name, path, surface):
+	def __init__(self, name, path, surface):
 
-        self.name = name
-        self.path = path
-        self.image = image
-        self.surface = surface
-        self.x = 50
-        self.y = 50
+		self.name = name
+		self.path = path
+		self.image = image
+		self.surface = surface
+		self.x = 50
+		self.y = 50
+		self.dx = 0
+		self.dy = 0
+		self.isJumping = False
+		self.doubleJump = False
+		self.timeSinceJump = 0
+		self.onPlatform = False
+	
+	def setOnPlatform(self, bool):
+		self.onPlatform = bool
+		
+	def setLastJump(self, time):
+		self.timeSinceJump = time
 
+	def draw(self):
+		self.surface.blit(self.image, (self.x,self.y))
 
-    def draw(self):
+	def moveX(self):
+		self.x += self.dx
 
-        self.surface.blit(self.image, (self.x,self.y))
-
-    def moveX(self, dx):
-        self.x += dx
-
-    def moveY(self, dy):
-        self.y += dy
-        
+	def moveY(self):
+		self.y += self.dy
+		
+	def setX(self, x):
+		self.x = x
+		
+	def setY(self, y):
+		self.y = y
+		
+	def setdX(self, dx):
+		self.dx = dx
+		
+	def setdY(self, dy):
+		self.dy = dy
+		
+	def getX(self):
+		return self.x
+		
+	def getY(self):
+		return self.y
+		
+	def changeY(self, deltay):
+		self.dy += deltay
+	
+	def changeX(self, deltax):
+		self.dx += deltax
+		
+	def move(self):
+		if self.y >= 390:
+			self.onPlatform = False
+		self.moveX()
+		self.moveY()
